f193e8ef98c61b971a48ce1d282707a2305e7ce1
idongessien <i.d.essien@gmail.com>
2025-05-31 04:58:47 +0000

    Update Phone Link

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -31,7 +31,7 @@ export default function TopBar() {
         <TopBarContainer>
             <TopBarUl>
                 <li>
-                    <A href="sms:+17743126471?body=Hey%20Idong,%20I%20was%20on%20your%20links%20site.%20Looking%20forward%20to%20speaking%20with%20you%20about%20..."><FontAwesomeIcon Icon="fa-solid fa-phone" /></A>
+                    <A href="sms:+17743126471?body=Hey%20Idong,%20I%20was%20on%20your%20links%20site.%20Looking%20forward%20to%20speaking%20with%20you%20about%20..."><i class="fa-solid fa-phone"></i></A>
                 </li>
                 <li>
                     <A href="mailto:idong.essien@comcast.net?subject=RE:%20All%20Your%20Links&body=Does%20this%20work?">e: idong.essien@comcast.net</A>
