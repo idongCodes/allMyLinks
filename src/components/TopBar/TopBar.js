bc32ebba54216819315200acf278e69a96ffed4a
idongessien <i.d.essien@gmail.com>
2025-05-31 05:21:18 +0000

    Import FA Component

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 export default function TopBar() {
@@ -31,7 +32,7 @@ export default function TopBar() {
         <TopBarContainer>
             <TopBarUl>
                 <li>
-                    <A href="sms:+17743126471?body=Hey%20Idong,%20I%20was%20on%20your%20links%20site.%20Looking%20forward%20to%20speaking%20with%20you%20about%20..."><i class="fa-solid fa-phone"></i></A>
+                    <A href="sms:+17743126471?body=Hey%20Idong,%20I%20was%20on%20your%20links%20site.%20Looking%20forward%20to%20speaking%20with%20you%20about%20..."><FontAwesomeIcon icon="fa-solid fa-phone" /></A>
                 </li>
                 <li>
                     <A href="mailto:idong.essien@comcast.net?subject=RE:%20All%20Your%20Links&body=Does%20this%20work?">e: idong.essien@comcast.net</A>
