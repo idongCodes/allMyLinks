d5649700ce437967a985c9316091aba3458f81b3
idongessien <i.d.essien@gmail.com>
2025-05-31 06:23:32 +0000

    FA Import Fix

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -32,10 +32,10 @@ export default function TopBar() {
         <TopBarContainer>
             <TopBarUl>
                 <li>
-                    <A href="sms:+17743126471?body=Hey%20Idong,%20I%20was%20on%20your%20links%20site.%20Looking%20forward%20to%20speaking%20with%20you%20about%20..."><FontAwesomeIcon icon={['fas', 'phone']} /></A>
+                    <A href="sms:+17743126471?body=Hey%20Idong,%20I%20was%20on%20your%20links%20site.%20Looking%20forward%20to%20speaking%20with%20you%20about%20..."><FontAwesomeIcon icon={['fas', 'phone']}/></A>
                 </li>
                 <li>
-                    <A href="mailto:idong.essien@comcast.net?subject=RE:%20All%20Your%20Links&body=Does%20this%20work?"><FontAwesomeIcon icon={['fas', 'envelope']} /></A>
+                    <A href="mailto:idong.essien@comcast.net?subject=RE:%20All%20Your%20Links&body=Does%20this%20work?"><FontAwesomeIcon icon={['fas', 'envelope']}/></A>
                 </li>
             </TopBarUl>
         </TopBarContainer>
